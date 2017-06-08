30b191b5feb171c8017c1990243ff1369e7d364a
Candy.Chen <779837984@qq.com>
2017-06-08 22:23:36 +0800

    first commit

---
+++ src/test/java/com/candy/test/CCWTest.java
@@ -0,0 +1,14 @@
+package com.candy.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by candy on 2017/6/8.
+ */
+public class CCWTest {
+    @Test
+    public void test(){
+        System.out.println("Hello World");
+
+    }
+}
